b3670ab500a3217b1b7227378467df5ade937cb6
geniusapc <princearthur06@gmail.com>
2020-12-09 14:27:43 +0100

    fix: removed mongoose vaalidation from cooperative payment

--- models/CooperativePayment.js
+++ models/CooperativePayment.js
@@ -5,37 +5,30 @@ const schema = new Schema(
   {
     userId: {
       type: String,
-      required: [true, 'userId is required'],
     },
     orderRef: {
       type: String,
-      required: [true, 'Payment order ref is required'],
     },
     invoice: {
       type: mongoose.Types.ObjectId,
-      required: [true, 'Invoice id is required'],
       ref: 'Coop_Invoice',
     },
     cooperativeId: {
       type: mongoose.Types.ObjectId,
-      required: [true, 'cooperative Id ref is required'],
       ref: 'Cooperative',
     },
     paymentRef: {
       type: String,
-      required: [true, 'Payment ref is required'],
     },
     code: {
       type: String,
     },
     amount: {
       type: Number,
-      required: [true, 'Payment amount is required'],
     },
     status: {
       type: String,
       lowercase: true,
-      required: [true, 'Payment status is required'],
     },
     currency: {
       type: String,
@@ -45,7 +38,6 @@ const schema = new Schema(
     paymentType: {
       type: String,
       enum: ['fcWallet', 'coopWallet', 'flutterwave'],
-      required: [true, 'Payment type is required'],
     },
     deliveryStatus: {
       type: String,
