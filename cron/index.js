890c10baf2d8be5d0f11497f0d6fb4ef7c5c4b62
geniusapc <princearthur06@gmail.com>
2020-12-20 08:40:20 +0100

    feat: change settlement scheduler to run on mondays

--- cron/index.js
+++ cron/index.js
@@ -3,7 +3,7 @@ const generateCoopPaymentInvoice = require('./generateCoopPaymentInvoice');
 const logger = require('../startup/logger');
 
 module.exports = async () => {
-  cron.schedule('* * * * Sunday', async () => {
+  cron.schedule('* * * * Monday', async () => {
     logger.info(`generating cooperative settlement invoice for Date:${Date()}`);
     generateCoopPaymentInvoice();
   });
