46fbb781910ea2280e218f5a7ae10212373e903f
geniusapc <princearthur06@gmail.com>
2020-12-21 22:57:12 +0100

    feat: changed cron to run on tuesdays

--- cron/index.js
+++ cron/index.js
@@ -3,7 +3,7 @@ const generateCoopPaymentInvoice = require('./generateCoopPaymentInvoice');
 const logger = require('../startup/logger');
 
 module.exports = async () => {
-  cron.schedule('0 12 * * Monday', async () => {
+  cron.schedule('0 12 * * Tuesday', async () => {
     logger.info(`Generating cooperative settlement invoice for Date:${Date()}`);
     generateCoopPaymentInvoice();
   });
