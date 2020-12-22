3cb00a1033d0c5422be6a0cd051929136aed239c
geniusapc <princearthur06@gmail.com>
2020-12-22 11:12:58 +0100

    feat: change invoice cron date

--- cron/index.js
+++ cron/index.js
@@ -3,7 +3,7 @@ const generateCoopPaymentInvoice = require('./generateCoopPaymentInvoice');
 const logger = require('../startup/logger');
 
 module.exports = async () => {
-  cron.schedule('0 12 * * Tuesday', async () => {
+  cron.schedule('0 12 * * Wednesday', async () => {
     logger.info(`Generating cooperative settlement invoice for Date:${Date()}`);
     generateCoopPaymentInvoice();
   });
